ecf559c42561c51b7fe367e00b7889ac143c5209
Adeyemi Adeniji <danieladeniji001@gmail.com>
2026-05-13 10:27:45 +0100

    add Demo.java with implementations of linear and binary search algorithms

---
+++ Demo.java
@@ -0,0 +1,37 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        int nums[] = {5, 7, 9, 11, 13};
+        int target = 7;
+
+        int result = binarySearch(nums, target);
+        System.out.println("Element found at index: " + result);
+    }
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
